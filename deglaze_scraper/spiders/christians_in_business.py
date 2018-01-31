0034147807cfc84e4c75904fd0bd43b60e71e23b
Michael Thiessen <electromikenetic@gmail.com>
2018-01-31 06:45:42 -0500

    Added some error handling to make it more robust

--- deglaze_scraper/spiders/christians_in_business.py
+++ deglaze_scraper/spiders/christians_in_business.py
@@ -2,6 +2,12 @@
 from scrapy import Spider, Request
 from scrapy.http import FormRequest
 
+def acc(arr, index):
+    if len(arr) >= index + 1:
+        return arr[index]
+    else:
+        return ''
+
 class ChristiansInBusinessSpider(Spider):
     name = 'christians_in_business'
     allowed_domains = ['www.christiansinbusiness.com']
@@ -16,16 +22,6 @@ class ChristiansInBusinessSpider(Spider):
             dont_filter=True
         )
 
-    # def handle_search_disclaimer(self, response):
-    #   print "Handling search disclaimer"
-
-    #   yield FormRequest(
-    #       'http://www.christiansinbusiness.com/search-disclaimer',
-    #       formdata={"iAgree": "true"},
-    #       callback=self.start_scraping,
-    #       dont_filter=True
-    #   )
-
     def clean_str_arr(self, arr):
         return filter(
             lambda str: str != '',
@@ -39,14 +35,13 @@ class ChristiansInBusinessSpider(Spider):
         print "Starting to scrape now"
         print response.headers
 
-        yield Request(
-            # 'http://www.christiansinbusiness.com/search?keyword=&location=&pageSize=100',
-            # 'http://www.christiansinbusiness.com/business/47979/guarantee-real-estate---patrick-kim',
-            'http://www.christiansinbusiness.com/business/43431/mccoy-blossom-funeral-home---moscow-mills',
-            cookies={"BusinessSearchAgree": "True"},
-            callback=self.parse_company_page,
-            dont_filter=True
-        )
+        for page in range(6, 21):
+            yield Request(
+                'http://www.christiansinbusiness.com/search?keyword=&location=&pageSize=100&pg={}'.format(page),
+                cookies={"BusinessSearchAgree": "True"},
+                callback=self.parse_search_results,
+                dont_filter=True
+            )
 
     def parse_search_results(self, response):
         print 'Scraping: {}'.format(response.url)
@@ -54,34 +49,41 @@ class ChristiansInBusinessSpider(Spider):
 
         companyUrls = response.xpath("//a[contains(text(),'Details')]/@href")
         for url in companyUrls:
-            print url.extract()
+            yield Request(
+                response.urljoin(url.extract()),
+                cookies={"BusinessSearchAgree": "True"},
+                callback=self.parse_company_page
+            )
 
     def parse_company_page(self, response):
         print 'Scraping company: {}'.format(response.url)
 
         company = {}
 
-        company['company_name'] = response.xpath('//h2[@class="page-title"]/a/text()').extract()[0]
+        page_titles = response.xpath('//h2[@class="page-title"]/a/text()').extract()
+        company['company_name'] = acc(page_titles, 0)
 
         context = response.xpath('//div[@class="agent"]')
         address_text = self.clean_str_arr(context.xpath('.//address/text()').extract())
 
-        company['address'] = address_text[0]
-        company['church'] = address_text[1]
+        company['address'] = acc(address_text, 0)
+        company['church'] = acc(address_text, 1)
 
         context = response.xpath('//div[@class="agent"]//div[@class="agent-contacts clearfix"]')
-        company['url'] = context.xpath('./a[text()="Visit our website"]/@href').extract()[0].lower()
+        company['url'] = acc(context.xpath('./a[text()="Visit our website"]/@href').extract(), 0).lower()
+
         social_media = context.xpath('./div[@class="social-media"]//li/a/@href').extract()
         company['social_media'] = ", ".join(social_media).lower()
 
         info_block_text = self.clean_str_arr(context.xpath('./div[@class="info-bar-block"]//text()').extract())
 
-        company['contact_name'] = info_block_text[2]
-        company['contact_title'] = info_block_text[4]
+        company['contact_name'] = acc(info_block_text, 2)
+        company['contact_title'] = acc(info_block_text, 4)
 
-        company['contact_email'] = context.xpath('./a[text() = "Contact Us"]/@href').extract()[0].split(':')[1].lower()
+        unprocessed_email = acc(context.xpath('./a[text() = "Contact Us"]/@href').extract(), 0)
+        company['contact_email'] = acc(unprocessed_email.split(':'), 1).lower()
 
-        print company
+        yield company
 
 
 
